fb0af95ebc908b7a519f75ad731883de19df5cc8
Wenhao Lv <kuro_des@icloud.com>
2024-05-14 20:51:23 +0800

    update arxiv

--- n8n_docker/scripts/arxiv_rss.py
+++ n8n_docker/scripts/arxiv_rss.py
@@ -1,3 +1,5 @@
+import json
+import os
 import feedparser
 import feedgenerator
 from datetime import datetime
@@ -5,19 +7,21 @@ import openai
 from time import sleep
 from typing import List
 
+# OPENAI_API_KEY = "sk-xxxxx"
 OPENAI_API_KEY = open("/home/scripts/openai_api_key").read().strip()
 openai.api_key = OPENAI_API_KEY
-# openai.api_key = "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"
 GPT3 = "gpt-3.5-turbo" 
 GPT4 = "gpt-4o",
 
 rss_file = '/home/files/arxiv.rss'
 # rss_file = "/Users/kuro/Desktop/coding/rss-everything/n8n_docker/files/arxiv.rss"
+history_file = '/home/files/history.json'
 
 # +cs.NI
 arxiv_urls = [
     "https://rss.arxiv.org/rss/cs.DB+cs.DC+cs.OS"
 ]
+focus_categories = ["cs.DB", "cs.DC", "cs.OS"]
 
 def getCompletion(messages, model=GPT3):
     try:
@@ -45,37 +49,59 @@ def main(urls: List[str]):
         image="https://arxiv.org/icons/sfx.gif"
     )
 
-    # decuplicate across multiple URL, inside each excution
-    unique_ids = set()
+    if not os.path.exists(history_file):
+        with open(history_file, 'w') as fp:
+            json.dump({"links": []}, fp)
+            fp.close()
+
+    with open(history_file, 'r') as fp:
+        history = json.load(fp)
+        fp.close()
 
     for feed in input_feeds:
         for item in feed.entries:
-            if item.link not in unique_ids:
+            # check the first category
+            if item.tags[0].term not in focus_categories:
+                continue
+            # check duplication
+            if item.link in history["links"]:
+                continue
 
-                # # chatgpt
-                # messages = [{"role": "system", "content" : "You are ChatGPT, a large language model trained by OpenAI. Answer as concisely as possible.\nKnowledge cutoff: 2021-09-01\nCurrent date: 2023-03-02"}]
-                # messages.append({"role": "user", "content": f"Translate this title of a computer science paper into Chinese: {item.title}"})
-                # title_translation = getCompletion(messages)
-                # messages.append({"role": "user", "content": f"As a new PhD candidate, I'm having difficulty understanding a computer science paper with the abstract provided below. Please help me by:\n1. Translating the abstract into Chinese.\n2. For each technical term used in the abstract, provide a detailed explanation in English, followed by its translation into Chinese. Organize your response in a list format, with each item containing the technical term, its explanation, and its Chinese translation.\n\n\"\"\"\n{item.summary}\n\"\"\""})
-                # abs_explanation = re.sub(r'\n', '<br>', getCompletion(messages))
-                # formated_description = f"<p>{title_translation}</p><p>{item.author}</p><p>{item.summary}</p><p>{abs_explanation}</p>"
+            history["links"].append(item.link)
+            if len(history["links"]) > 1000:
+                history["links"] = history["links"][-1000:]
 
-                abstract = item.summary.split("Abstract: ")[1]
-                formated_description = f"<p>{item.author}</p><p>{abstract}</p>"
+            abstract = item.summary.split("Abstract: ")[1]
+            terms = ", ".join([tag.term for tag in item.tags])
 
-                # add to feed
-                output_feed.add_item(
-                    title=item.title,
-                    link=item.link,
-                    description=formated_description,
-                    author_name=item.author,
-                    # A string that uniquely identifies the item.
-                    unique_id=item.link,
-                )
-                unique_ids.add(item.link)
+            # # chatgpt
+            # messages = [{"role": "system", "content" : "You are a helpful assistant. You can help me by answering my questions. You can also ask me questions."}]
+            # # messages.append({"role": "user", "content": f"Translate this title of a computer science paper into Chinese: {item.title}"})
+            # # title_translation = getCompletion(messages, GPT4)
+            # messages.append({"role": "user", "content": f"Translate this abstract of a computer science paper into Chinese:\n\n\"\"\"\n{abstract}\n\"\"\""})
+            # # messages.append({"role": "user", "content": f"As a new PhD candidate, I'm having difficulty understanding a computer science paper with the abstract provided below. Please help me by:\n1. Translating the abstract into Chinese.\n2. For each technical term used in the abstract, provide a detailed explanation in English, followed by its translation into Chinese. Organize your response in a list format, with each item containing the technical term, its explanation, and its Chinese translation.\n\n\"\"\"\n{abstract}\n\"\"\""})
+            # # abs_explanation = re.sub(r'\n', '<br>', getCompletion(messages))
+            # abs_translation = getCompletion(messages, GPT3)
+            abs_translation = ''
+
+            formated_description = f"<p>{item.author}</p><p>{terms}</p><p>{abstract}</p><p>{abs_translation}</p>"
+
+            # add to feed
+            output_feed.add_item(
+                title=item.title,
+                link=item.link,
+                description=formated_description,
+                author_name=item.author,
+                # A string that uniquely identifies the item.
+                unique_id=item.link,
+            )
 
     # xml_string = output_feed.writeString("utf-8")
     # print(xml_string)
+
+    with open(history_file, 'w') as fp:
+        json.dump(history, fp)
+        fp.close()
 
     with open(rss_file, 'w') as fp:
         output_feed.write(fp, 'utf-8')
